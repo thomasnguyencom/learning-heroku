b98def3654b3a848a7289d6f4bdf52d4edaabdbb
thomasnguyencom <contact@thomasnguyen.com>
2012-02-29 00:42:40 -0500

    removing to start over

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Helloworld::Application.config.secret_token = 'aeeb50c09a1fdbb33bb455c4fe1252f66e4d4e5f4cc7bb0e5fd267e9bfee11fb1ab21ef07323087c6298689e0ac30cda689b2cab4f6c2194a0bbe019746a914e'
